fbcbcda72920f32a29e2b237ddda68fa9f928f84
Artem Gorbatiuk <artem.gorbatiuk@netcracker.com>
2022-04-18 11:15:03 +0400

    fix: 7.4

--- src/main/java/org/example/seventh/four/FourthMain.java
+++ src/main/java/org/example/seventh/four/FourthMain.java
@@ -1,18 +1,26 @@
 package org.example.seventh.four;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class FourthMain {
-  public static void main(String[] args) {
-    final List<Shirt> shirts = new ArrayList<Shirt>() {{
-      add(new Shirt("111", "some1", "red", "1000"));
-      add(new Shirt("222", "some2", "blue", "999"));
-      add(new Shirt("333", "some3", "white", "333"));
-      add(new Shirt("444", "some4", "black", "20020"));
-    }};
 
-    System.out.println(shirts.toString());
+    private static final String[] inputShirts = {
+        "111, some1, red, 1000",
+        "222, some2, blue, 999",
+        "333, some3, white, 333",
+        "444, some4, black, 20020",
+    };
 
-  }
+    public static void main(String[] args) {
+        final Shirt[] shirts = new Shirt[inputShirts.length];
+        for (int i = 0; i < inputShirts.length; i++) {
+            try {
+                shirts[i] = new Shirt(inputShirts[i]);
+            } catch (ShirtException sh) {
+                sh.printStackTrace();
+            }
+        }
+        System.out.println("Shirts:\n");
+        for (Shirt shirt : shirts) {
+            System.out.println(shirt.toString());
+        }
+    }
 }
--- src/main/java/org/example/seventh/four/Shirt.java
+++ src/main/java/org/example/seventh/four/Shirt.java
@@ -1,21 +1,36 @@
 package org.example.seventh.four;
 
 public class Shirt {
-  private String id;
-  private String description;
-  private String color;
-  private String size;
+    private String id;
+    private String description;
+    private String color;
+    private String size;
 
-  public Shirt(String id, String description, String color, String size) {
-    this.id = id;
-    this.description = description;
-    this.color = color;
-    this.size = size;
-  }
+    public Shirt(final String inputShirt) {
+        if (inputShirt == null || inputShirt.isEmpty()) {
+            throw new ShirtException("Incorrect input!", inputShirt);
+        }
+        final String[] shirtInfo = inputShirt.split(", ");
+        if (shirtInfo.length != Shirt.class.getDeclaredFields().length) {
+            throw new ShirtException("Some field isn't exist!", inputShirt);
+        }
+        this.id = shirtInfo[0];
+        this.description = shirtInfo[1];
+        this.color = shirtInfo[2];
+        this.size = shirtInfo[3];
+    }
 
-
-  @Override
-  public String toString() {
-    return String.format("\n\"%s, %s, %s, %s\"\n", id, description, color, size);
-  }
+    @Override
+    public String toString() {
+        return "Shirt{" +
+            "id='" + id + '\'' +
+            ", description='" + description + '\'' +
+            ", color='" + color + '\'' +
+            ", size='" + size + '\'' +
+            '}';
+    }
+//    @Override
+//    public String toString() {
+//        return String.format("\n\"%s, %s, %s, %s\"\n", id, description, color, size);
+//    }
 }
