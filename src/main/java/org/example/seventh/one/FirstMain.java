16198d48bb0071e3e882ba7edc76647bb3a965d9
Artem Gorbatiuk <artem.gorbatiuk@netcracker.com>
2022-04-18 10:54:00 +0400

    feature: added 7.1

--- src/main/java/org/example/seventh/one/FirstMain.java
+++ src/main/java/org/example/seventh/one/FirstMain.java
@@ -1,7 +1,36 @@
 package org.example.seventh.one;
 
 public class FirstMain {
+
+  private static final String[] queries = {
+      "1, Артём, Горбатюк, ПМИб, 0",
+      "1, Кто-то, Кто-тов, ПМИб, 17",
+      "1, Никто, Никтотов, ПМИб, 2",
+  };
   public static void main(String[] args) {
-    // todo: wtf? 7.1 - strange task
+    final int allowedDebts = 7;
+    final TStudent[] students = new TStudent[queries.length];
+    for (int i = 0; i < queries.length; i++) {
+      try {
+        students[i] = new TStudent(queries[i]);
+      } catch (RuntimeException re) {
+        re.printStackTrace();
+      }
+    }
+    final TGroup[] groups = new TGroup[students.length];
+    for (int i = 0; i < students.length; i++) {
+      if (students[i].getNumberOfDebt() > allowedDebts) {
+        groups[i] = new TGroup(
+            students[i].getStudentId(),
+            students[i].getFirstName(),
+            students[i].getLastName(),
+            students[i].getGroupId()
+        );
+      }
+    }
+    System.out.println("Должники:\n");
+    for (int i = 0; i < students.length; i++) {
+      System.out.println(groups[i]);
+    }
   }
 }
---
+++ src/main/java/org/example/seventh/one/TStudent.java
@@ -0,0 +1,48 @@
+package org.example.seventh.one;
+
+// entity
+public class TStudent {
+    private String studentId;
+    private String firstName;
+    private String lastName;
+    private String groupId;
+    private Integer numberOfDebt;
+
+    public TStudent(final String inputStudent) {
+        if (inputStudent == null || inputStudent.isEmpty()) {
+            throw new RuntimeException("Incorrect info!");
+        }
+        final String[] studentInfo = inputStudent.split(", ");
+        if (studentInfo.length != 5) {
+            throw new RuntimeException("Some student's data is not exist!");
+        }
+        this.studentId = studentInfo[0];
+        this.firstName = studentInfo[1];
+        this.lastName = studentInfo[2];
+        this.groupId = studentInfo[3];
+        this.numberOfDebt = Integer.valueOf(studentInfo[4]);
+    }
+
+    public TStudent() {
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public Integer getNumberOfDebt() {
+        return numberOfDebt;
+    }
+}
